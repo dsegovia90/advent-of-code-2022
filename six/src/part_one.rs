aef7ba414d0f088a8bf861be23b5854671059e78
Daniel Segovia <segoarce90@gmail.com>
2022-12-06 11:29:11 -0800

    formatting

--- six/src/part_one.rs
+++ six/src/part_one.rs
@@ -1,12 +1,11 @@
 use std::collections::HashSet;
 
 pub fn calculate(s: String) -> usize {
-
     for idx in 0..s.len() - 4 {
         let substring = &s[idx..idx + 4];
         let set: HashSet<_> = HashSet::from_iter(substring.chars());
         if set.len() == 4 {
-            return idx + 4
+            return idx + 4;
         }
     }
 
@@ -22,7 +21,13 @@ mod test {
         assert_eq!(7, calculate("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string()));
         assert_eq!(5, calculate("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string()));
         assert_eq!(6, calculate("nppdvjthqldpwncqszvftbrmjlhg".to_string()));
-        assert_eq!(10, calculate("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string()));
-        assert_eq!(11, calculate("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string()));
+        assert_eq!(
+            10,
+            calculate("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string())
+        );
+        assert_eq!(
+            11,
+            calculate("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string())
+        );
     }
 }
--- six/src/part_two.rs
+++ six/src/part_two.rs
@@ -1,12 +1,11 @@
 use std::collections::HashSet;
 
 pub fn calculate(s: String) -> usize {
-
     for idx in 0..s.len() - 14 {
         let substring = &s[idx..idx + 14];
         let set: HashSet<_> = HashSet::from_iter(substring.chars());
         if set.len() == 14 {
-            return idx + 14
+            return idx + 14;
         }
     }
 
@@ -22,7 +21,13 @@ mod test {
         assert_eq!(19, calculate("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string()));
         assert_eq!(23, calculate("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string()));
         assert_eq!(23, calculate("nppdvjthqldpwncqszvftbrmjlhg".to_string()));
-        assert_eq!(29, calculate("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string()));
-        assert_eq!(26, calculate("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string()));
+        assert_eq!(
+            29,
+            calculate("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string())
+        );
+        assert_eq!(
+            26,
+            calculate("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string())
+        );
     }
 }
