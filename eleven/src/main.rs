4996febb317ffdb80052889439a10f7a7ffd2784
Daniel Segovia <segoarce90@gmail.com>
2022-12-12 19:07:19 -0800

    least common denominator

---
+++ eleven/src/main.rs
@@ -0,0 +1,10 @@
+mod part_one;
+mod part_two;
+
+
+fn main() {
+    let result_1 = part_one::calculate();
+    println!("{}", result_1);
+    let result_2 = part_two::calculate();
+    println!("{}", result_2);
+}
---
+++ eleven/src/part_two.rs
@@ -0,0 +1,144 @@
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+struct Monkey {
+    items: VecDeque<u64>,
+    operation: fn(u64) -> u64,
+    throw_to_index: fn(u64) -> usize,
+    inspects: u64,
+}
+
+pub fn calculate() -> u64 {
+    let monkey0 = Monkey {
+        items: VecDeque::from([89, 73, 66, 57, 64, 80]),
+        inspects: 0,
+        operation: |old| (old * 3) % 9699690,
+        throw_to_index: |item| {
+            if item % 13 == 0 {
+                return 6;
+            }
+            2
+        },
+    };
+    let monkey1 = Monkey {
+        items: VecDeque::from([83, 78, 81, 55, 81, 59, 69]),
+        inspects: 0,
+        operation: |old| (old + 1) % 9699690,
+        throw_to_index: |item| {
+            if item % 3 == 0 {
+                return 7;
+            }
+            4
+        },
+    };
+    let monkey2 = Monkey {
+        items: VecDeque::from([76, 91, 58, 85]),
+        inspects: 0,
+        operation: |old| (old * 13) % 9699690,
+        throw_to_index: |item| {
+            if item % 7 == 0 {
+                return 1;
+            }
+
+            4
+        },
+    };
+    let monkey3 = Monkey {
+        items: VecDeque::from([71, 72, 74, 76, 68]),
+        inspects: 0,
+        operation: |old| (old * old) % 9699690,
+        throw_to_index: |item| {
+            if item % 2 == 0 {
+                return 6;
+            }
+
+            0
+        },
+    };
+    let monkey4 = Monkey {
+        items: VecDeque::from([98, 85, 84]),
+        inspects: 0,
+        operation: |old| (old + 7) % 9699690,
+        throw_to_index: |item| {
+            if item % 19 == 0 {
+                return 5;
+            }
+
+            7
+        },
+    };
+    let monkey5 = Monkey {
+        items: VecDeque::from([78]),
+        inspects: 0,
+        operation: |old| (old + 8) % 9699690,
+        throw_to_index: |item| {
+            if item % 5 == 0 {
+                return 3;
+            }
+
+            0
+        },
+    };
+    let monkey6 = Monkey {
+        items: VecDeque::from([86, 70, 60, 88, 88, 78, 74, 83]),
+        inspects: 0,
+        operation: |old| (old + 4) % 9699690,
+        throw_to_index: |item| {
+            if item % 11 == 0 {
+                return 1;
+            }
+
+            2
+        },
+    };
+    let monkey7 = Monkey {
+        items: VecDeque::from([81, 58]),
+        inspects: 0,
+        operation: |old| (old + 5) % 9699690,
+        throw_to_index: |item| {
+            if item % 17 == 0 {
+                return 3;
+            }
+
+            5
+        },
+    };
+
+    let mut monkeys = [monkey0, monkey1, monkey2, monkey3, monkey4, monkey5, monkey6, monkey7];
+
+    for _ in 0..10000 {
+        for idx in 0..monkeys.len() {
+            let monkey = monkeys.get_mut(idx).unwrap();
+            let mut moves: Vec<(usize, u64)> = Vec::new();
+
+            while !monkey.items.is_empty() {
+                monkey.inspects += 1;
+                let item = monkey.items.pop_front().unwrap();
+                let item = (monkey.operation)(item);
+                let monkey_index = (monkey.throw_to_index)(item);
+                moves.push((monkey_index, item))
+            }
+
+            moves.iter().for_each(|(monkey_index, item)| {
+                let monkey = monkeys.get_mut(*monkey_index).unwrap();
+                monkey.items.push_back(*item)
+            })
+        }
+    }
+
+    monkeys.sort_by(|a, b| b.inspects.cmp(&a.inspects));
+
+    monkeys.iter().take(2).fold(1, |acc, monkey| {
+        monkey.inspects * acc
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::calculate;
+
+    #[test]
+    fn base_case() {
+        assert_eq!(calculate(), 119715);
+    }
+}
